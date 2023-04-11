61a98afc10f900e91caad3e22725082820130594
Sandeep <Sandeep@echoinnovateit.com>
2023-04-11 12:22:08 +0530

    Hello World

---
+++ app/src/main/java/com/example/customfooter/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.customfooter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.customfooter.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+    }
+}
